96e6f86ea485f56df8556372948f054b689979f0
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-15 20:03:12 +0300

    Foo.java

--- src/a/b/c/Foo.java
+++ src/a/b/c/Foo.java
@@ -4,7 +4,7 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 5/14/2018.
  * Project: r12
- * ********************
+
  * *******************************
  */
 public class Foo {
