e1b296df13161061176108b1fa91f8169ff02090
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-15 16:45:13 +0300

    change author name (using replace)

--- src/a/b/c/Foo2.java
+++ src/a/b/c/Foo2.java
@@ -2,7 +2,7 @@ package a.b.c;
 
 /**
  * *******************************
- * Created by Irina.Petrovskaya on 5/18/2018.
+ * Created by irina on 5/18/2018.
  * Project: r12
  * *******************************
  */
